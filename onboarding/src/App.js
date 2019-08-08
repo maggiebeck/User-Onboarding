0cc9bfa105f7a81c9755c2c0a45990dee16d3002
Margaret <mpbeck@umich.edu>
2019-08-07 20:25:29 -0400

    mvp

---
+++ onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Form from './components/form'
+
+
+function App() {
+  return (
+    <div>
+   <h1>Create an Account</h1>
+    <Form />
+    </div>
+  );
+}
+ export default App;
